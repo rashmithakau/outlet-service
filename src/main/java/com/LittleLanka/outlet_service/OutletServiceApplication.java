0d1507c70777c17089eac46023724230544ca4a1
Rashmitha Kaushalya <151978018+rashmithakau@users.noreply.github.com>
2024-12-18 07:59:42 +0530

    Intialized the project

---
+++ src/main/java/com/LittleLanka/outlet_service/OutletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.LittleLanka.outlet_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutletServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutletServiceApplication.class, args);
+	}
+
+}
