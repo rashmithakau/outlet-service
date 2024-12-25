1a541bc83ed1330e55339652ff43e81c27f0fffb
Indrajith <deegalla12378@usci.ruh.ac.lk>
2024-12-25 07:21:30 +0530

    create Repositories for each entity class

---
+++ src/main/java/com/LittleLanka/outlet_service/repository/FactoryOrderRepository.java
@@ -0,0 +1,6 @@
+package com.LittleLanka.outlet_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FactoryOrderRepository extends JpaRepository <OrderId, Integer> {
+}
