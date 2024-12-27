dd18896e697b1677e39cea611f0f4463cd2b5e5c
Indrajith <deegalla12378@usci.ruh.ac.lk>
2024-12-27 21:40:38 +0530

    created a endpoint for save outlet

---
+++ src/main/java/com/LittleLanka/outlet_service/service/OutletService.java
@@ -0,0 +1,7 @@
+package com.LittleLanka.outlet_service.service;
+
+import com.LittleLanka.outlet_service.dto.OutletDTO;
+
+public interface OutletService {
+    public OutletDTO saveOutlet(OutletDTO outletDTO);
+}
