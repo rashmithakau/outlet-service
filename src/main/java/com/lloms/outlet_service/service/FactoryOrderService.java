58a1317ef8ebd66168866e24f78477a2d5cae1c5
Rashmitha Kaushalya <151978018+rashmithakau@users.noreply.github.com>
2025-03-03 15:16:22 +0530

    creted endpoint for save fac order

---
+++ src/main/java/com/lloms/outlet_service/service/FactoryOrderService.java
@@ -0,0 +1,7 @@
+package com.lloms.outlet_service.service;
+
+import com.lloms.outlet_service.dto.request.FactoryOrderRequestDTO;
+
+public interface FactoryOrderService {
+    public void saveFacOrder(FactoryOrderRequestDTO facOrdReq);
+}
