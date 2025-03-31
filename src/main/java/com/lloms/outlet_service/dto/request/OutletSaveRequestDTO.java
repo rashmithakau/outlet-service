bf7787b6c85758ad3ddc4e3e11c5b9377c6f5916
Thilina Ishan <151310264+ThilinaIshan@users.noreply.github.com>
2025-03-31 13:13:47 +0530

    update the save outlet function

--- src/main/java/com/lloms/outlet_service/dto/request/OutletSaveRequestDTO.java
+++ src/main/java/com/lloms/outlet_service/dto/request/OutletSaveRequestDTO.java
@@ -11,6 +11,7 @@ import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -22,4 +23,5 @@ public class OutletSaveRequestDTO {
     private String location;
     private int userId;
     private OutletStatus status;
+    private MultipartFile imageFile;
 }
--- src/main/java/com/lloms/outlet_service/service/OutletService.java
+++ src/main/java/com/lloms/outlet_service/service/OutletService.java
@@ -1,17 +1,21 @@
 package com.lloms.outlet_service.service;
 
+import com.lloms.outlet_service.dto.OutletDTO;
 import com.lloms.outlet_service.dto.request.OutletSaveRequestDTO;
 import com.lloms.outlet_service.dto.request.RequestUpdateOutletDTO;
 import com.lloms.outlet_service.dto.response.ResponseGetOutletDTO;
+import org.springframework.core.io.Resource;
 
 import java.util.List;
 
 public interface OutletService {
-    String saveOutlet(OutletSaveRequestDTO outletSaveRequestDTO);
+    OutletDTO saveOutlet(OutletSaveRequestDTO outletSaveRequestDTO);
 
     ResponseGetOutletDTO getOutletById(int outletId);
 
     List<ResponseGetOutletDTO> getAllOutlets();
 
     ResponseGetOutletDTO updateOutlet(RequestUpdateOutletDTO requestUpdateOutletDTO, int outletID);
+
+    Resource getImageByUrl(String url);
 }
