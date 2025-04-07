89967b41f77f58d7070aa7f6887642dc0375fa3c
Thilina Ishan <151310264+ThilinaIshan@users.noreply.github.com>
2025-04-08 01:11:05 +0530

    create outlet endpoint for owner approval

--- src/main/java/com/lloms/outlet_service/repository/OutletRepository.java
+++ src/main/java/com/lloms/outlet_service/repository/OutletRepository.java
@@ -1,12 +1,16 @@
 package com.lloms.outlet_service.repository;
 
 import com.lloms.outlet_service.entity.Outlet;
+import com.lloms.outlet_service.enums.OutletStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @EnableJpaRepositories
 public interface OutletRepository extends JpaRepository <Outlet, Integer> {
+    List<Outlet> findAllByStatusEquals(OutletStatus outletStatus);
 
 }
