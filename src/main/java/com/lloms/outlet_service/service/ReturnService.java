9f3ab4d95fee5e328c282ac866aa1b7bac7775f5
Rashmitha Kaushalya <151978018+rashmithakau@users.noreply.github.com>
2025-04-08 16:25:51 +0530

    final

--- src/main/java/com/lloms/outlet_service/service/ReturnService.java
+++ src/main/java/com/lloms/outlet_service/service/ReturnService.java
@@ -1,8 +1,21 @@
 package com.lloms.outlet_service.service;
 
 import com.lloms.outlet_service.dto.OutletReturnDTO;
+import com.lloms.outlet_service.enums.OutletReturnStatus;
 import org.modelmapper.internal.bytebuddy.asm.Advice;
+
+import java.util.List;
 
 public interface ReturnService {
     void saveReturn (OutletReturnDTO returnDTO);
+
+    void updateReturnStatus(Integer returnId, OutletReturnStatus status);
+
+    List<OutletReturnDTO> getAllReturns();
+
+    List<OutletReturnDTO> getAllReturnsByStatus(OutletReturnStatus status);
+
+    List<OutletReturnDTO> getAllReturnsByOutletId(Integer outletId);
+
+    List<OutletReturnDTO> getAllReturnsNotPending();
 }
--- src/main/java/com/lloms/outlet_service/service/impl/ReturnServiceImpl.java
+++ src/main/java/com/lloms/outlet_service/service/impl/ReturnServiceImpl.java
@@ -1,9 +1,11 @@
 package com.lloms.outlet_service.service.impl;
 
 import com.lloms.outlet_service.dto.OutletReturnDTO;
+import com.lloms.outlet_service.dto.ReturnItemDto;
 import com.lloms.outlet_service.entity.Outlet;
 import com.lloms.outlet_service.entity.OutletReturn;
 import com.lloms.outlet_service.entity.ReturnItem;
+import com.lloms.outlet_service.enums.OutletReturnStatus;
 import com.lloms.outlet_service.repository.OutletRepository;
 import com.lloms.outlet_service.repository.OutletReturnRepository;
 import com.lloms.outlet_service.service.ReturnService;
@@ -12,6 +14,8 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -47,5 +51,144 @@ public class ReturnServiceImpl implements ReturnService {
         // Save OutletReturn with return items (CascadeType.ALL handles saving items)
        outletReturnRepository.save(outletReturn);
     }
+
+    @Override
+    public void updateReturnStatus(Integer returnId, OutletReturnStatus status) {
+        OutletReturn outletReturn =outletReturnRepository.findByreturnId(returnId);
+        outletReturn.setOutletReturnStatus(status);
+        outletReturnRepository.save(outletReturn);
+    }
+
+    @Override
+    public List<OutletReturnDTO> getAllReturns() {
+        List<OutletReturn> listOutletsReturns = outletReturnRepository.findAllWithItems(); // use new method
+        List<OutletReturnDTO> outletReturnDTOs = new ArrayList<>();
+
+        listOutletsReturns.forEach(outletReturn -> {
+            OutletReturnDTO outletReturnDTO = new OutletReturnDTO();
+            outletReturnDTO.setReturnId(outletReturn.getReturnId());
+            outletReturnDTO.setReturnDate(outletReturn.getReturnDate());
+            outletReturnDTO.setOutletId(outletReturn.getOutlet().getOutletId());
+            outletReturnDTO.setOutletReturnStatus(outletReturn.getOutletReturnStatus());
+            outletReturnDTO.setOutletName(outletReturn.getOutlet().getOutletName());
+
+            List<ReturnItemDto> returnItems = outletReturn.getReturnItems().stream()
+                    .map(ety -> {
+                        ReturnItemDto item = new ReturnItemDto();
+                        item.setProductId(ety.getProductId());
+                        item.setQuantity(ety.getQuantity());
+                        item.setReason(ety.getReason());
+                        item.setProductName(ety.getProductName());
+                        item.setUnitPrice(ety.getUnitPrice());
+                        return item;
+                    }).collect(Collectors.toList());
+
+            outletReturnDTO.setReturnItems(returnItems);
+            outletReturnDTOs.add(outletReturnDTO);
+        });
+
+        return outletReturnDTOs;
+    }
+
+    @Override
+    public List<OutletReturnDTO> getAllReturnsByStatus(OutletReturnStatus status) {
+        List<OutletReturn> listOutletsReturns = outletReturnRepository.findAllByOutletReturnStatus(status); // use new method
+        List<OutletReturnDTO> outletReturnDTOs = new ArrayList<>();
+
+        listOutletsReturns.forEach(outletReturn -> {
+            OutletReturnDTO outletReturnDTO = new OutletReturnDTO();
+            outletReturnDTO.setReturnId(outletReturn.getReturnId());
+            outletReturnDTO.setReturnDate(outletReturn.getReturnDate());
+            outletReturnDTO.setOutletId(outletReturn.getOutlet().getOutletId());
+            outletReturnDTO.setOutletReturnStatus(outletReturn.getOutletReturnStatus());
+            outletReturnDTO.setOutletName(outletReturn.getOutlet().getOutletName());
+
+            List<ReturnItemDto> returnItems = outletReturn.getReturnItems().stream()
+                    .map(ety -> {
+                        ReturnItemDto item = new ReturnItemDto();
+                        item.setProductId(ety.getProductId());
+                        item.setQuantity(ety.getQuantity());
+                        item.setReason(ety.getReason());
+                        item.setProductName(ety.getProductName());
+                        item.setUnitPrice(ety.getUnitPrice());
+                        return item;
+                    }).collect(Collectors.toList());
+
+            outletReturnDTO.setReturnItems(returnItems);
+            outletReturnDTOs.add(outletReturnDTO);
+        });
+
+        return outletReturnDTOs;
+    }
+
+    @Override
+    public List<OutletReturnDTO> getAllReturnsByOutletId(Integer outletId) {
+        List<OutletReturn> listOutletsReturns = outletReturnRepository.getAllByOutletWithItems(outletRepository.getReferenceById(outletId));
+// use new method
+        List<OutletReturnDTO> outletReturnDTOs = new ArrayList<>();
+
+        listOutletsReturns.forEach(outletReturn -> {
+            OutletReturnDTO outletReturnDTO = new OutletReturnDTO();
+            outletReturnDTO.setReturnId(outletReturn.getReturnId());
+            outletReturnDTO.setReturnDate(outletReturn.getReturnDate());
+            outletReturnDTO.setOutletId(outletReturn.getOutlet().getOutletId());
+            outletReturnDTO.setOutletReturnStatus(outletReturn.getOutletReturnStatus());
+            outletReturnDTO.setOutletName(outletReturn.getOutlet().getOutletName());
+
+            List<ReturnItemDto> returnItems = outletReturn.getReturnItems().stream()
+                    .map(ety -> {
+                        ReturnItemDto item = new ReturnItemDto();
+                        item.setProductId(ety.getProductId());
+                        item.setQuantity(ety.getQuantity());
+                        item.setReason(ety.getReason());
+                        item.setProductName(ety.getProductName());
+                        item.setUnitPrice(ety.getUnitPrice());
+                        return item;
+                    }).collect(Collectors.toList());
+
+            outletReturnDTO.setReturnItems(returnItems);
+            outletReturnDTOs.add(outletReturnDTO);
+        });
+
+        return outletReturnDTOs;
+    }
+
+    @Override
+    public List<OutletReturnDTO> getAllReturnsNotPending() {
+        List<OutletReturnStatus> statuses = Arrays.asList(
+                OutletReturnStatus.Approved,
+                OutletReturnStatus.Rejected
+        );
+
+        List<OutletReturn> listOutletsReturns = outletReturnRepository.findReturnsWithItemsNotPending(); // use the correct JPA query method
+        List<OutletReturnDTO> outletReturnDTOs = new ArrayList<>();
+
+        for (OutletReturn outletReturn : listOutletsReturns) {
+            OutletReturnDTO outletReturnDTO = new OutletReturnDTO();
+            outletReturnDTO.setReturnId(outletReturn.getReturnId());
+            outletReturnDTO.setReturnDate(outletReturn.getReturnDate());
+            outletReturnDTO.setOutletId(outletReturn.getOutlet().getOutletId());
+            outletReturnDTO.setOutletReturnStatus(outletReturn.getOutletReturnStatus());
+            outletReturnDTO.setOutletName(outletReturn.getOutlet().getOutletName());
+
+            List<ReturnItemDto> returnItems = outletReturn.getReturnItems().stream()
+                    .map(ety -> {
+                        ReturnItemDto item = new ReturnItemDto();
+                        item.setProductId(ety.getProductId());
+                        item.setQuantity(ety.getQuantity());
+                        item.setReason(ety.getReason());
+                        item.setProductName(ety.getProductName());
+                        item.setUnitPrice(ety.getUnitPrice());
+                        return item;
+                    }).collect(Collectors.toList());
+
+            outletReturnDTO.setReturnItems(returnItems);
+            outletReturnDTOs.add(outletReturnDTO);
+        }
+
+        return outletReturnDTOs;
+    }
+
+
 
 }
